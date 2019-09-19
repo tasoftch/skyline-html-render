6544ffc24a2d37a530f84a125f62d72248006be5
Thomas Abplanalp <info@tasoft.ch>
2019-09-19 11:02:01 +0200

    FIX: Add special component compilation

--- src/Compiler/ConfigCompilerHelper.php
+++ src/Compiler/ConfigCompilerHelper.php
@@ -46,8 +46,10 @@ class ConfigCompilerHelper extends StandardFactoryCompiler
                             if(!$cg)
                                 $collected["#"] = $cg = new Config();
                             $cg[$tg] = $file;
+
                             $cc = $conf[ AbstractComponent::COMP_ELEMENT_ARGUMENTS ];
                             unset($cc["file"]);
+                            $conf[ AbstractComponent::COMP_ELEMENT_ARGUMENTS ] = $cc;
                         }
                     }
                     $cfg[$key] = $iterator($conf);
